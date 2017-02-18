61136919440c2e0d34e43651964f9bc5c5b97bba
kuwar <kuwarsaurav21@gmail.com>
2017-02-18 09:53:34 +0545

    about and weather component added

---
+++ app/components/About.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Weather = React.createClass({
+  render: function () {
+    return (
+      <h1>About Components</h1>
+    );
+  }
+});
+
+module.exports = Weather;
