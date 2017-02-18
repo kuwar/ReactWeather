adc57b9f8ccfc8d1579b2e4b2d7871f8a5bb1e9d
kuwar <kuwarsaurav21@gmail.com>
2017-02-18 22:09:52 +0545

    api call to openweatermap added

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+//const OPEN_WEATHER_MAP_URL = 'http://samples.openweathermap.org/data/2.5/weather?appid=9e4b0bb643beee25ad1455f9d6965652&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=9e4b0bb643beee25ad1455f9d6965652&units=imperial';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios(requestUrl).then(function(res) {
+      if (res.data.cod && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function(res) {
+      throw new Error(res.data.message);
+    });
+  }
+};
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,27 +1,47 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var openWeatherMap = require('openWeatherMap');
 
 var About = React.createClass({
   getInitialState: function () {
     return ({
-      location: "Miame",
-      temp: 88
+      isLoading: false
     });
   },
   handleSearch: function (location) {
+    var that = this;
     this.setState({
-      location: location,
-      temp: 23
+      isLoading: true
+    })
+    openWeatherMap.getTemp(location).then(function(temp) {
+      that.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+    }, function(errorMessage) {
+      alert(errorMessage);
+      that.setState({
+        isLoading: false
+      });
     });
   },
   render: function () {
-    var {temp, location} = this.state;
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>Fetching weather ...</h3>
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
     return (
       <div>
         <h1>Weather Components</h1>
         <WeatherForm onSearch={this.handleSearch}/>
-        <WeatherMessage temp={temp} location={location}/>
+        {renderMessage()}
       </div>
     );
   }
