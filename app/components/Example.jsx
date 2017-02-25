24de367f397cb0c1e5e5da13ff231f825845290c
kuwar <kuwarsaurav21@gmail.com>
2017-02-25 15:49:38 +0545

    foundation added to example page

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => {
   return (
     <div>
-      <h2>Example</h2>
-      <p>Welcome to example page!</p>
+      <h1 className="text-center">Example</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=philadelphia">Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to="/?location=Rio">Rio, Brazil</Link>
+        </li>
+      </ol>
     </div>
   );
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,11 @@ var Main = (props) => {
   return (
     <div>
       <Nav/>
-      <h1>Main components</h1>
-      {props.children}
+      <div className="row">
+        <div className="column medium-6 large-4 small-centered">
+          {props.children}
+        </div>
+      </div>
     </div>
   );
 };
