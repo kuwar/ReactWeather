dd6fb9df7a6eecc7b1b06b75c280a15fee75970d
kuwar <kuwarsaurav21@gmail.com>
2017-03-05 08:50:32 +0545

    = added

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -18,7 +18,7 @@ var ErrorModal = React.createClass({
     var {title, message} = this.props;
 
     return (
-      <div id="error-modal" className="reveal tiny text-center" data-reveal>
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
         <p>
